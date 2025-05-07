363f63cbb8b33e17109e92c71b101f9d84a8a713
Amund Kiste <amund@kiste.no>
2025-05-07 10:02:41 +0200

    Add basic readout system

---
+++ readout.ino
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+
+
+void setup()
+{
+	analogReadResolution(16); // Set ADC resolution to 16 bits
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(1000000, SERIAL_8N1);
+	digitalWrite(LED_BUILTIN, HIGH);
+	while(!Serial); // Wait for Serial Monitor to open
+	Serial.flush();
+	digitalWrite(LED_BUILTIN, LOW);
+
+
+}
+
+bool led_state = false;
+
+constexpr uint32_t frequency = 1u; // Hz
+constexpr uint32_t period = 1000000u/frequency; // us
+
+constexpr double adc_max = (double)(2e12);
+bool dir = true;
+void loop()
+{
+	uint32_t start = micros();
+	digitalWrite(LED_BUILTIN, led_state=!led_state);
+	Serial.print("A0:");
+	int a0 = analogRead(A0);
+	double a0f = (((double)a0)/adc_max) * 3.3d;
+	Serial.print(a0);
+	Serial.print(" ");
+	Serial.println(a0f);
+	Serial.print("A1:");
+	int a1 = analogRead(A1);
+	double a1f = (((double)a1)/adc_max) * 3.3d;
+	Serial.print(a1);
+	Serial.print(" ");
+	Serial.println(a1f);
+	Serial.print("A2:");
+	int a2 = analogRead(A2);
+	double a2f = (((double)a2)/adc_max) * 3.3d;
+	Serial.print(a2);
+	Serial.print(" ");
+	Serial.println(a2f);
+	Serial.print("A3:");
+	int a3 = analogRead(A3);
+	double a3f = (((double)a3)/adc_max) * 3.3d;
+	Serial.print(a3);
+	Serial.print(" ");
+	Serial.println(a3f);
+	Serial.flush();
+	uint32_t elapsed = micros() - start;
+	if (elapsed < period)
+	{
+		delayMicroseconds(period - elapsed);
+	}
+}
